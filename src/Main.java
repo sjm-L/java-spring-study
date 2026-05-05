359aecbdb4039eefba0d74b41fc4a8d275f0a58b
jm <shower1300@gmail.com>
2026-05-06 00:47:57 +0900

    study: 생성자 기본 예제 작성

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        Book book = new Book("자바 입문", 15000);
-        book.printInfo();
+        Member member = new Member("김정민", 25);
+        member.printInfo();
     }
 }
-class Book {
-    String title;
-    int price;
+class Member {
+    String name;
+    int age;
 
-    Book(String title, int price){
-        this.title = title;
-        this.price = price;
+    Member(String name, int age){
+        this.name = name;
+        this.age = age;
     }
     void printInfo(){
-        System.out.println(title + "책의 가격은 " + price + "입니다.");
+        System.out.println("이름: " + name + "나이: "  + age);
     }
 }
